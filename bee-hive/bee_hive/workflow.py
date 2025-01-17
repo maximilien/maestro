c100404327913066fe3a82936dcfb59548b4c4d0
Akihiko (Aki) Kuroda <16141898+akihikokuroda@users.noreply.github.com>
2025-01-17 12:51:25 -0500

    Update sequence workflow (#123)
    * feat(hive): sequence workflow
    
    Signed-off-by: Akihiko Kuroda <akihikokuroda2020@gmail.com>

--- bee-hive/bee_hive/workflow.py
+++ bee-hive/bee_hive/workflow.py
@@ -9,37 +9,38 @@ import yaml
 import dotenv
 
 dotenv.load_dotenv()
+from agent import Agent
 
 class Workflow:
     agents = []
-    def __init__(self, agent_defs)
+    workflow = {}
+    def __init__(self, agent_defs, workflow):
         """Execute sequential workflow.
         input:
             agents: array of agent definitions
+            workflow: workflow definition
         """
         for agent_def in agent_defs:
-            agents.append(new Agent(agent_def))
+            self.agents.append(Agent(agent_def))
+        self.workflow = workflow
 
 
-    def sequential_workflow(workflow):
-        """Execute sequential workflow.
-        input: workflow
-            workflow["spec"]["template"]["prompt"]: prompt for the agent
-            workflow["spec"]["template"]["agents"]: array of agent instance
-        """
-    
-        prompt = workflow["spec"]["template"]["prompt"]
-        agents = workflow["spec"]["template"]["agents"]
-        if (
-            workflow_yaml["spec"]["strategy"]["output"]
-            and workflow_yaml["spec"]["strategy"]["output"] == "verbose"
-           ):
-            run_workflow = run_streaming_agent
+    def run(self):
+        """Execute workflow."""
+
+        if (self.workflow["spec"]["strategy"]["type"]  == "sequence"):
+            return self._sequence()
         else:
-            run_workflow = run_agent
-        for agent in agents:
-            prompt = run_workflow(agent[agent, prompt)
+            print("not supported yet")   
+
+    def _sequence(self):
+        prompt = self.workflow["spec"]["template"]["prompt"]
+        for agent in self.agents:
+            if (
+                self.workflow["spec"]["strategy"]["output"]
+                and self.workflow["spec"]["strategy"]["output"] == "verbose"
+            ):
+                prompt = agent.run_streaming(prompt)
+            else:
+                prompt = agent.run(prompt)
         return prompt
-
-
-
