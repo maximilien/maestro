8c9c2ba2c48d42ca3649fafe771095f7d303c08a
Paul Schweigert <paul@paulschweigert.com>
2025-02-06 14:26:53 -0500

    drop framework and related files (#191)
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

--- examples/bee_agent/tools/weather.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-from bee_agent import BeeAgent, LLM
-from bee_agent.tools import WeatherTool
-
-agent = BeeAgent(llm=LLM(model="llama3.1"), tools=[WeatherTool()])
-
-agent.run("What's the current weather in Las Vegas?")
--- framework/bee-ts/README.md
+++
@@ -1,3 +0,0 @@
-> COMING SOON
-
-The Typescript implementation of the Bee Agent Framework is available at: https://github.com/i-am-bee/bee-agent-framework
