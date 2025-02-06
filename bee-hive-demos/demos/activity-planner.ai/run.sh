e3f3ebb48c83d3eafd83adb543eadd6cb8484e43
Angelo Danducci <angelo.danducci.ii@ibm.com>
2025-02-06 09:31:21 -0500

    unify organization structure of demos (#187)

---
+++ bee-hive-demos/demos/activity-planner.ai/run.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+# TODO automation of the run is pending the workflow schema and agent/workflow split work
---
+++ bee-hive-demos/demos/summary.ai/run.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+# TODO break down the hive shell script into two pieces - commands required to run the demo go here
