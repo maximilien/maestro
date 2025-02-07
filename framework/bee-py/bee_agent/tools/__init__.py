cb14310a9589dc4fc20d89c94434ff22e3edc757
Paul Schweigert <paul@paulschweigert.com>
2025-02-07 14:13:55 -0500

    Drop framework pt 2 (#199)
    * update readme
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * remove bee-py files
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    ---------
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

--- framework/bee-py/bee_agent/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-from .weather import WeatherTool
-from .tool import Tool
-from .mcp_tools import MCPTool, MCPToolOutput, MCPToolInput
-
-__all__ = ["WeatherTool", "Tool", "MCPTool", "MCPToolOutput", "MCPToolInput"]
--- framework/bee-py/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-"""Pytest configuration for asyncio testing."""
-
-import pytest
-
-
-def pytest_addoption(parser):
-    """Add pytest command line options."""
-    parser.addini("asyncio_mode", "default mode for async fixtures", default="strict")
