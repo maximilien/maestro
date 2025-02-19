d66fc12cbd50b9d4646749c1bcf9ab81e7306b90
dr.max <maxim@us.ibm.com>
2025-02-19 13:53:46 -0800

    fixes issue #116 (#243)
    * does not call run.sh but test.sh instead since runs as --dry-run
    * doctor.sh does only checks maestro is there but more could be done
    * need to add other demos

--- maestro/demos/workflows/weather-checker.ai/test.sh
+++ maestro/demos/workflows/weather-checker.ai/test.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
+
+function check_status() {
+    if [ $? -ne 0 ]; then
+      echo $1
+    fi
+}
+
 echo "validate 🗒️ agents.yaml"
 maestro validate ../../../schemas/agent_schema.json ./agents.yaml
+check_status "failed to validate agents.yaml ❌"
 
 echo "validate 🗒️ workflow.yaml"
 maestro validate ../../../schemas/workflow_schema.json  ./workflow.yaml
+check_status "failed to validate worflow.yaml ❌"
 
 echo "run 🏃🏽‍♂️‍➡️ workflow.yaml"
-../maestro run ./agents.yaml ./workflow.yaml
+maestro run --dry-run ./agents.yaml ./workflow.yaml
+check_status "failed to run worflow ❌"
