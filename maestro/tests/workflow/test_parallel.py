a2d98a4d6accedbae13698252fcbf451916e3f43
Akihiko (Aki) Kuroda <16141898+akihikokuroda@users.noreply.github.com>
2025-02-27 09:57:44 -0500

    Add test of  loop and parallel (#270)

---
+++ maestro/tests/workflow/test_parallel.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# Copyright © 2025 IBM
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os, yaml
+import pytest
+from unittest import TestCase
+from src.workflow import Workflow
+
+import asyncio
+
+def parse_yaml(file_path):
+    with open(file_path, "r") as file:
+        yaml_data = list(yaml.safe_load_all(file))
+    return yaml_data
+
+# `parallel` tests
+class TestParallel(TestCase):
+    def setUp(self):        
+        self.agents_yaml = parse_yaml(os.path.join(os.path.dirname(__file__),"../yamls/agents/dry_run_paralle_agent.yaml"))
+        self.workflow_yaml = parse_yaml(os.path.join(os.path.dirname(__file__),"../yamls/workflows/parallel_workflow.yaml"))
+        try:
+            self.workflow = Workflow(self.agents_yaml, self.workflow_yaml[0])
+        except Exception as excep:
+            raise RuntimeError("Unable to create agents") from excep
+        
+    def tearDown(self):
+        self.workflow = None
+
+    def test_parallel(self):
+        response = asyncio.run(self.workflow.run())
+        assert "['test2', 'test3', 'test4']" in response["final_prompt"]
+
+if __name__ == '__main__':
+    unittest.main()
