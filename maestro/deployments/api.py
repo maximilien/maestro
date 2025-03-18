875f143d5e45146b47608fa01271bcd119ab9434
Akihiko (Aki) Kuroda <16141898+akihikokuroda@users.noreply.github.com>
2025-03-18 09:47:51 -0400

    Streaming output (#319)

--- maestro/deployments/api.py
+++ maestro/deployments/api.py
@@ -1,25 +1,55 @@
 # Assisted by watsonx Code Assistant
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, Response
 import os
 import json
 import sys
 import io
 import asyncio
+import threading
 
 import yaml
 from src.workflow import Workflow
 
 app = Flask(__name__)
-history = ""
+output = io.StringIO()
+workflow_instance = None
+thread = None
+position = 0
 
 def parse_yaml(file_path):
     with open(file_path, "r") as file:
         yaml_data = list(yaml.safe_load_all(file))
     return yaml_data
 
+def generate():
+    global output
+    global position
+    global thread
+    message = output.getvalue()
+    if len(message) > position:
+        text = "data: " + output.getvalue()[position:].replace("\n", "  \ndata: ")+ "\n\n"
+        yield text
+    position = len(message)
+    #if thread and not thread.is_alive():
+    #    text = "data: Thread completed EndEnd\n\n"
+    #    yield text
+
+@app.route('/stream')
+def stream():
+    return Response(generate(), mimetype='text/event-stream')
+
+def start_workflow():
+    global workflow_instance
+    global output
+    sys.stdout = output
+    asyncio.run(workflow_instance.run())
+
 @app.route('/', methods=['GET'])
 def process_workflow():
-    global history
+    global workflow_instance
+    global output
+    global thread
+    global position
     if request.method == 'GET':
         agents_yaml = parse_yaml("src/agents.yaml")
         workflow_yaml = parse_yaml("src/workflow.yaml")
@@ -30,24 +60,18 @@ def process_workflow():
             workflow_instance = Workflow(agents_yaml, workflow_yaml[0])
         except Exception as excep:
             raise RuntimeError("Unable to create agents") from excep
+
         diagram = workflow_instance.to_mermaid()
+
         clear = request.args.get("Clear Output")
         if clear:
-            history = ""
+            output = io.StringIO()
+        position = 0
+        thread = threading.Thread(target=start_workflow)
+        thread.start()
 
-        output = io.StringIO()
-        sys.stdout = output
-        asyncio.run(workflow_instance.run())
-        sys.stdout = sys.__stdout__
+        name = workflow_yaml[0]["metadata"]["name"]
+        return render_template('index.html', result="", title=name, diagram=diagram)
 
-        response = {
-            'output': output.getvalue()
-        }
-        if os.getenv("DEBUG"):
-            return response
-        else:
-            history = history + output.getvalue()
-            name = workflow_yaml[0]["metadata"]["name"]
-            return render_template('index.html', result=history, title=name, diagram=diagram)
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
