950f64728ab9cc6e8d4e720dc94cc10923898561
dr.max <mmaximilien@gmail.com>
2025-03-28 07:45:33 -0700

    Update __init__.py
    fix typo
    
    Signed-off-by: dr.max <mmaximilien@gmail.com>

--- maestro/src/__init__.py
+++ maestro/src/__init__.py
@@ -18,7 +18,7 @@ dotenv.load_dotenv()
 
 from .workflow import Workflow
 
-from .agents.beeai_agent import BeeAIA
+from .agents.beeai_agent import BeeAI
 from .agents.remote_agent import RemoteAgent
 
 from .agents.agent import save_agent, restore_agent, remove_agent
