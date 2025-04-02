328b1ef90b8d410fda15856a91ce54f0e2dbf82b
Akihiko (Aki) Kuroda <16141898+akihikokuroda@users.noreply.github.com>
2025-04-02 13:09:24 -0400

    add latest bee agent support (#383)
    * add latest bee agent support
    
    Signed-off-by: Akihiko Kuroda <akihikokuroda2020@gmail.com>

--- maestro/src/agents/agent_factory.py
+++ maestro/src/agents/agent_factory.py
@@ -3,11 +3,13 @@ from enum import Enum
 from typing import Callable, Type, Union
 
 from .beeai_agent import BeeAIAgent
+from .beeai_local_agent import BeeAILocalAgent
 from .crewai_agent import CrewAIAgent
 
 class AgentFramework(Enum):
     """Enumeration of supported frameworks"""
     BEEAI = "beeai"
+    BEEAILOCAL = "beeailocal"
     CREWAI = "crewai"
 
 class AgentFactory:
@@ -24,6 +26,7 @@ class AgentFactory:
         """
         factories = {
             AgentFramework.BEEAI: BeeAIAgent,
+            AgentFramework.BEEAILOCAL: BeeAILocalAgent,
             AgentFramework.CREWAI: CrewAIAgent
         }
 
@@ -33,6 +36,6 @@ class AgentFactory:
         return factories[framework]
 
     @classmethod
-    def get_factory(cls, framework: str) -> Callable[..., Union[BeeAIAgent, CrewAIAgent]]:
+    def get_factory(cls, framework: str) -> Callable[..., Union[BeeAIAgent, BeeAILocalAgent, CrewAIAgent]]:
         """Get a factory function for the specified agent type."""
         return cls.create_agent(framework)
