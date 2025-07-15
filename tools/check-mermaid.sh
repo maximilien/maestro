7604fcd93ae180adc3779f4985a34b7f49eb3a5d
dr.max <maxim@us.ibm.com>
2025-07-14 18:37:25 -0700

    bug: fixes issue #616 (#617)

--- tools/check-mermaid.sh
+++ tools/check-mermaid.sh
@@ -8,8 +8,8 @@ then
 fi
 
 declare -i fail=0
-WORKFLOW_FILES=$(find . -path "./.venv" -prune -o -name '*workflow*.yaml' -print)
-AGENT_FILES=$(find . -path "./.venv" -prune -o -name '*agents*.yaml' -print)
+WORKFLOW_FILES=$(find . -path "./.venv" -prune -o -path "./.github/workflows" -prune -o -name '*workflow*.yaml' -print)
+AGENT_FILES=$(find . -path "./.venv" -prune -o -path "./.github/workflows" -prune -o -name '*agents*.yaml' -print)
 EXCLUDE_FILES=("./tests/yamls/workflowrun/simple_workflow_run.yaml"
 	       "./operator/config/crd/bases/maestro.ai4quantum.com_workflowruns.yaml"
 	       "./operator/config/crd/bases/maestro.ai4quantum.com_workflows.yaml"
